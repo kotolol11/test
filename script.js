6c8af4266a20bd2c9fd3ebf3d752404016d66d3a
Kourion <Kotolol11@hotmail.com>
2020-06-17 19:30:35 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
